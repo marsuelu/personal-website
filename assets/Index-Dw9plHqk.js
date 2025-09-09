7441ee61c256121da5d80255a2d41d7f1fd4542a
ManshuLu <79955308+marsuelu@users.noreply.github.com>
2025-09-09 16:00:17 +0800

    Updates

---
+++ assets/Index-Dw9plHqk.js
@@ -0,0 +1,2 @@
+import{j as r}from"./index-Byjm7hrW.js";import"./router-ndYPTExO.js";import"./i18n-Dhs0AlZX.js";import"./vendor-DJG_os-6.js";const e=()=>r.jsx("div",{className:"min-h-1vh bg-fuchsia-400",children:"Travel"});export{e as default};
+//# sourceMappingURL=Index-Dw9plHqk.js.map
